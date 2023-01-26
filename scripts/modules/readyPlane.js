f4b4285869b42eaa44a00d72c0cee41104c53547
BorysDudnyk <100314965+BorysDudnyk@users.noreply.github.com>
2023-01-26 19:50:42 +0200

    Third сommit

--- scripts/modules/readyPlane.js
+++ scripts/modules/readyPlane.js
@@ -1,7 +1,27 @@
+import airplane from "./airplane.js";
+
 const readyPlane = (forms, main) => {
-    forms.forEach((form) => [
-        
-    ])
+    const data = [];
+
+    forms.forEach((form) => {
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+
+            for(const element of form.elements){
+                element.disabled = true;
+            }
+
+            data.push({
+                name: form.name.value,
+                ticket: form.ticket.value,
+            });
+
+            if (forms.length === data.length) {
+                forms.forEach(form => form.remove());
+                airplane(main, data);
+            }
+        });
+    })
 };
 
 
